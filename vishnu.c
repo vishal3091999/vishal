2d38a7b4c0ef1f60027a0151f1b10c8eb78c9201
vishal3091999 <vishalmv1999@gmail.com>
2017-06-30 14:06:41 +0530

    Update vishnu.c

--- vishnu.c
+++ vishnu.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("enter the numbers");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b)
+{
+printf(" a is greter");
+}
+if(a>c)
+{
+printf(" b is greter");
+}
+getch();
+}
+
 

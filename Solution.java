43c0922afe7bf061a2abac32d16d1697da31c4e4
vishal3091999 <vishalmv1999@gmail.com>
2019-02-01 12:35:31 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class Solution
+{
+  public static void main(String args[])
+  throws Exception {
+  Bufferedreader br=new Bufferedreader(new InputStreamReader(System.in));
+  String line =br.readline();
+  int N=Integer.parseInt(line);
+  for(int i=0;i<N;i++)
+  {
+    System.out.println("hello word");
+    }
+   }
+  }
+ 

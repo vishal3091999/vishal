3d31444f62183d2ce530bfbb8e5d96996dea6dc8
vishal3091999 <vishalmv1999@gmail.com>
2019-02-01 12:22:04 +0530

    Create positive_negative.java

---
+++ positive_negative.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Positive_Negative
+{
+  public static void main(String[]args)
+  {
+    int n;
+    Scanner s=new Scanner(System.in);
+    System.out.println("enter the number you want to check:");
+    n=s.nextInt();
+    if(n>0)
+    {
+      System.out.println("the given number "+n+"is postive");
+      }
+      else if(n<o)
+      {
+        System.out.println("the given number"+n+" is negative");
+        }
+        else
+        {
+          System.out.println("the given number "+n+" is neither positive or negative");
+          }
+        }
+       }
+        

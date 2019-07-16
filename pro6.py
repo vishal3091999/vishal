d27ab4a8b26bbe2984a7ee5ffbc0fce805d5fd93
vishal3091999 <vishalmv1999@gmail.com>
2019-07-16 14:54:02 +0530

    Create pro6.py

---
+++ pro6.py
@@ -0,0 +1,12 @@
+    
+n1=int(input())
+a=list(map(int,input().split()))
+c=0
+for i in range(0,n1-2):
+	for j in range(1,n1-1):
+		for k in range(2,n1):
+			if(a[i]<a[j] and a[j]<a[k]):
+      
+				c+=1
+        
+print(c)
